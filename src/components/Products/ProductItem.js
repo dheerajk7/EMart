66c1102e20bf7622ea61369ec9193ea93ec1b4fc
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-08-22 17:48:25 +0530

    add editable component

---
+++ src/components/Products/ProductItem.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+
+class ProductItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      price: '',
+      id: '',
+      title: '',
+      rating: '',
+      detail: '',
+      image: '',
+    };
+  }
+
+  componentDidMount() {
+    const { id, title, rating, price, detail, image } = this.props.product;
+    this.setState({ id, title, rating, price, detail, image });
+  }
+
+  handleChange = (label, value) => {
+    if (label === 'detail') {
+      this.setState({ detail: value });
+    } else if (label === 'title') {
+      this.setState({ title: value });
+    } else if (label === 'rating') {
+      this.setState({ rating: value });
+    } else if (label === 'price') {
+      this.setState({ price: value });
+    }
+  };
+
+  handleCancle = () => {
+    console.log('handleCancle');
+  };
+
+  handleSubmit = (id) => {
+    console.log(this.state);
+  };
+
+  render() {
+    const ratingArray = [1, 2, 3, 4, 5];
+    let edit = true;
+    const { id, title, rating, price, detail, image } = this.state;
+    return (
+      <div className="product-item-container" key={id}>
+        {!edit && (
+          <div className="container-div">
+            <div className="image">
+              <img src={image} alt={title}></img>
+            </div>
+            <div className="product-detail">
+              <div className="title">{title}</div>
+              <div className="detail">{detail}</div>
+              <div className="rating">
+                {ratingArray.map((value) => {
+                  if (value <= rating) {
+                    return (
+                      <i
+                        className="fa fa-star"
+                        aria-hidden="true"
+                        key={value}
+                      ></i>
+                    );
+                  }
+                  return <span key={value}></span>;
+                })}
+              </div>
+            </div>
+            <div className="option-container">
+              <div className="price">
+                <div>Price:</div>
+                <div className="tag">Rs {price}</div>
+              </div>
+              <div className="buttons">
+                <i className="fa fa-pencil"></i>
+                <i className="fa fa-trash"></i>
+              </div>
+            </div>
+          </div>
+        )}
+        {edit && (
+          <div className="container-div">
+            <div className="image">
+              <img src={image} alt={title}></img>
+            </div>
+            <div className="product-detail">
+              <div className="title">
+                <input
+                  type="text"
+                  value={title}
+                  required
+                  onChange={(event) => {
+                    this.handleChange('title', event.target.value);
+                  }}
+                />
+              </div>
+              <div className="detail">
+                <textarea
+                  type="text"
+                  value={detail}
+                  data-label="detail"
+                  required
+                  onChange={(event) => {
+                    this.handleChange('detail', event.target.value);
+                  }}
+                />
+              </div>
+              <div className="rating">
+                {
+                  <input
+                    type="number"
+                    value={rating}
+                    required
+                    onChange={(event) => {
+                      this.handleChange('rating', event.target.value);
+                    }}
+                  />
+                }
+              </div>
+            </div>
+            <div className="option-container">
+              <div className="price">
+                <div>Price:</div>
+                <div className="tag">
+                  Rs{' '}
+                  <input
+                    type="text"
+                    value={price}
+                    required
+                    onChange={(event) => {
+                      this.handleChange('price', event.target.value);
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="buttons">
+                <i className="fa fa-times" onClick={this.handleCancle}></i>
+                <i
+                  className="fa fa-check"
+                  onClick={() => {
+                    this.handleSubmit(id);
+                  }}
+                ></i>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default ProductItem;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,8 +1,40 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import ProductItem from './ProductItem';
 
 class Products extends Component {
   render() {
-    return <div>Products</div>;
+    let product = [
+      {
+        id: '123',
+        title: 'Mango',
+        price: 1222,
+        rating: 3,
+        image:
+          'https://hips.hearstapps.com/del.h-cdn.co/assets/cm/15/10/54f9207ab7c5b_-_kesar_mango.jpg?fill=160:190&resize=480:*',
+        detail:
+          'This is product. This is product. This is product. This is product. This is product. This is product. This is product. This is product. This is product. This is product. This is product. This is product. ',
+      },
+      {
+        id: '321',
+        title: 'Apple',
+        price: 2000,
+        rating: 4,
+        image:
+          'https://media.istockphoto.com/photos/red-apple-picture-id184276818?k=6&m=184276818&s=612x612&w=0&h=Fm2-2w98ahP4jUkj3UjgPa-dIqEHlRrsTxdA_a-Cclk=',
+        detail:
+          'This is product. This is product. This is product. This is product. This is product. This is product. This is product. This is product. This is product. This is product. This is product. This is product. ',
+      },
+    ];
+    return (
+      <div className="products-container">
+        <div className="products-heading">
+          <div className="heading">Products </div>
+          <div className="sort-price">Sort By Price</div>
+        </div>
+        <ProductItem product={product[0]} />
+        <ProductItem product={product[1]} />
+      </div>
+    );
   }
 }
 
